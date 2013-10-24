c77fddc744af210f460b9bf6fae0a1ee0ea24acb
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2013-10-25 00:45:17 +0200

    link extraction finished

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -1,8 +1,13 @@
 #include "downloader.h"
 
 #include <QtCore/QDebug>
+#include <QtCore/QRegExp>
+#include <QtCore/QString>
+#include <QtCore/QStringList>
 
 #include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
 
 class Downloader::Private
 {
@@ -20,7 +25,49 @@ Downloader::Downloader(const QString &downloadUrl, QObject* parent)
     d->netManager = new QNetworkAccessManager(this);
     d->downloadUrl = downloadUrl;
 
-    qDebug() << "download url: " << d->downloadUrl;
+    if (d->downloadUrl.split('/').last() != "read") {
+        d->downloadUrl += "/read";
+    }
+
+    // initiate download
+    QNetworkReply *reply = d->netManager->get(QNetworkRequest(d->downloadUrl));
+
+    connect(reply, &QNetworkReply::finished, [this, reply] () {
+        QByteArray rcv = reply->readAll();
+        reply->deleteLater();
+
+        QRegExp regex("window.params.thumbs");
+        QRegExp endRegex("function update_params");
+
+        int startIndex = regex.indexIn(rcv);
+        int endIndex = endRegex.indexIn(rcv);
+
+        QByteArray midStr = rcv.mid(startIndex, (endIndex - startIndex));
+
+        QList<QByteArray> images = midStr.trimmed().split('=').last().trimmed()
+                                        .replace('"', "")
+                                        .replace('\\', "")
+                                        .replace("thumbs", "images")
+                                        .replace("thumb.","")
+                                        .split(',');
+
+
+        QList<QByteArray> imageUrls;
+        for (QByteArray imageUrl : images) {
+            // clean
+            if (imageUrl.at(0) == '[') {
+                imageUrl.remove(0, 1);
+            }
+
+            if (imageUrl.at(imageUrl.count()-1) == ';') {
+                imageUrl.remove((imageUrl.count()-2), 2);
+            }
+
+            imageUrls << imageUrl;
+        }
+
+        qDebug() << "finals" << imageUrls;
+    });
 }
 
 
